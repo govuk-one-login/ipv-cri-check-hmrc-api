62aade38ce5e74ac9c18ebd47cefd8cd7b8e25e9
surajkumar <suraj.kumar@digital.cabinet-office.gov.uk>
2023-09-18 21:27:44 +0100

    BAU: Moved auth code handler to correct folder

--- lambdas/matching/tests/create-auth-code-handler.test.ts
+++ lambdas/matching/tests/create-auth-code-handler.test.ts
@@ -1,6 +1,6 @@
-import { TimeHandler } from "../src/time-handler";
+import { TimeHandler } from "../../issue-credential/src/time-handler";
 import { Context } from "aws-lambda";
-import { TimeEvent } from "../src/time-event";
+import { TimeEvent } from "../../issue-credential/src/time-event";
 import { CreateAuthCodeHandler } from "../src/create-auth-code-handler";
 
 describe("create-auth-code-handler", () => {
