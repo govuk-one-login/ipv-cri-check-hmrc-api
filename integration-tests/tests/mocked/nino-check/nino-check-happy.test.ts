8830f55565bf138785f3d6f7195725768a774881
surajkumar <suraj.kumar@digital.cabinet-office.gov.uk>
2023-09-26 17:16:44 +0100

    BAU: Refactoring and cleaning up tests

---
+++ integration-tests/tests/mocked/nino-check/nino-check-happy.test.ts
@@ -0,0 +1,38 @@
+import { HistoryEvent } from "@aws-sdk/client-sfn";
+import { SfnContainerHelper } from "./sfn-container-helper";
+
+jest.setTimeout(30_000);
+
+describe("nino-check-happy", () => {
+  let sfnContainer: SfnContainerHelper;
+
+  beforeAll(async () => {
+    sfnContainer = new SfnContainerHelper();
+  });
+
+  afterAll(async () => sfnContainer.shutDown());
+
+  it("has a step-function docker container running", async () => {
+    expect(sfnContainer.getContainer()).toBeDefined();
+  });
+
+  it.each([
+    ["with no previous attempt", "HappyPathTestNoPreviousAttempt"],
+    ["after 1 failed previous attempt", "HappyPathTestOn2ndAttempt"],
+  ])("should succeed when called %s", async (_, happyPath: string) => {
+    const input = JSON.stringify({
+      nino: "AA000003D",
+      sessionId: "12345",
+    });
+    const responseStepFunction = await sfnContainer.startStepFunctionExecution(
+      happyPath,
+      input
+    );
+    const results = await sfnContainer.waitFor(
+      (event: HistoryEvent) =>
+        event?.stateExitedEventDetails?.name === "Nino check successful",
+      responseStepFunction
+    );
+    expect(results[0].stateExitedEventDetails?.output).toEqual("{}");
+  });
+});
