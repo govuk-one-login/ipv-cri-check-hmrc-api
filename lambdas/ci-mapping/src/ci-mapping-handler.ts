f8f67892f2706a95d7ef6b40a6dd51b16f8c8682
surajkumar <suraj.kumar@digital.cabinet-office.gov.uk>
2023-12-21 13:23:10 +0000

    OJ-2213: Created Lambda to parse CRI Mappings

--- lambdas/ci-mapping/src/ci-mapping-handler.ts
+++ lambdas/ci-mapping/src/ci-mapping-handler.ts
@@ -1,9 +1,37 @@
 import { LambdaInterface } from "@aws-lambda-powertools/commons";
+import { CiMappingEvent } from "./ci-mapping-event";
+import { Logger } from "@aws-lambda-powertools/logger";
+
+const logger = new Logger();
 
 export class CiMappingHandler implements LambdaInterface {
-  public async handler(_event: unknown, _context: unknown): Promise<string> {
-    return "Hello, World!";
+  public async handler(
+    event: CiMappingEvent,
+    _context: unknown
+  ): Promise<Array<string>> {
+    try {
+      return getCIsForHmrcErrors(event);
+    } catch (error: unknown) {
+      const message = error instanceof Error ? error.message : String(error);
+      logger.error(`Error in CiMappingHandler: ${message}`);
+      throw error;
+    }
   }
+}
+
+function getCIsForHmrcErrors(event: CiMappingEvent): Array<string> {
+  const hmrcErrors = event.hmrc_errors.split(",");
+  const ciMappings = event.ci_mapping.split("||");
+  return Array.from(
+    new Set(
+      ciMappings.flatMap((ci) => {
+        const [ciKey, ciValue] = ci.split(":");
+        return hmrcErrors
+          .filter((hmrcError) => ciKey.includes(hmrcError))
+          .map(() => ciValue);
+      })
+    )
+  );
 }
 
 const handlerClass = new CiMappingHandler();
--- lambdas/ci-mapping/tests/ci-mapping-handler.test.ts
+++ lambdas/ci-mapping/tests/ci-mapping-handler.test.ts
@@ -1,10 +1,58 @@
 import { CiMappingHandler } from "../src/ci-mapping-handler";
 import { Context } from "aws-lambda";
+import { CiMappingEvent } from "../src/ci-mapping-event";
+
+const testCiMapping = "aaaa:ci_1||bbbb,cccc,dddd:ci_2||eeee,ffff,gggg:ci_3";
 
 describe("ci-mapping-handler", () => {
-  it("should print Hello, World!", async () => {
+  it("should return the correct CI for a given input", async () => {
+    const event = {
+      ci_mapping: testCiMapping,
+      hmrc_errors: "aaaa",
+    } as CiMappingEvent;
     const ciMappingHandler = new CiMappingHandler();
-    const result = await ciMappingHandler.handler({}, {} as Context);
-    expect(result).toStrictEqual("Hello, World!");
+
+    const result = await ciMappingHandler.handler(event, {} as Context);
+
+    expect(result).toEqual(["ci_1"]);
+  });
+
+  it.each(["bbbb", "cccc"])("should return correct CI value", async (input) => {
+    const event = {
+      ci_mapping: testCiMapping,
+      hmrc_errors: input,
+    } as CiMappingEvent;
+    const ciMappingHandler = new CiMappingHandler();
+
+    const result = await ciMappingHandler.handler(event, {} as Context);
+
+    expect(result).toEqual(["ci_2"]);
+  });
+
+  it.each(["eeee", "eeee,ffff", "eeee,ffff,gggg", "ffff", "gggg", "eeee,gggg"])(
+    "should return correct CI value in the same group for when errors",
+    async (input) => {
+      const event = {
+        ci_mapping: testCiMapping,
+        hmrc_errors: input,
+      } as CiMappingEvent;
+      const ciMappingHandler = new CiMappingHandler();
+
+      const result = await ciMappingHandler.handler(event, {} as Context);
+
+      expect(result).toEqual(["ci_3"]);
+    }
+  );
+
+  it("should return multiple CIs when they are different", async () => {
+    const event = {
+      ci_mapping: testCiMapping,
+      hmrc_errors: "aaaa,gggg",
+    } as CiMappingEvent;
+    const ciMappingHandler = new CiMappingHandler();
+
+    const result = await ciMappingHandler.handler(event, {} as Context);
+
+    expect(result).toEqual(["ci_1", "ci_3"]);
   });
 });
