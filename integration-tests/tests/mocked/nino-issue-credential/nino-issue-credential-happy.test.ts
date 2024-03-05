8aee5743266d418a789594dbcd2013248de7ca04
Olakunle Jegede <olakunle.jegede@digital.cabinet-office.gov.uk>
2024-03-05 11:42:03 +0000

    OJ-2380: Fix vc mock integration test

--- integration-tests/tests/mocked/nino-issue-credential/nino-issue-credential-happy.test.ts
+++ integration-tests/tests/mocked/nino-issue-credential/nino-issue-credential-happy.test.ts
@@ -7,38 +7,53 @@ const decode = (value: string) =>
   Buffer.from(value, "base64").toString("utf-8");
 
 const expectedPayload = {
-  iss: "dummy-value",
-  jti: expect.any(String),
-  sub: "test",
   vc: {
+    evidence: [
+      {
+        type: "IdentityCheck",
+        strengthScore: 2,
+        validityScore: 0,
+        failedCheckDetails: [
+          {
+            checkMethod: "data",
+          },
+        ],
+        ci: ["N01"],
+        txn: "d3bae729-127e-4a1a-be5b-0dcec68f9dc9",
+      },
+    ],
+    credentialSubject: {
+      socialSecurityRecord: [
+        {
+          personalNumber: "AA000003D",
+        },
+      ],
+      name: [
+        {
+          nameParts: [
+            {
+              type: "GivenName",
+              value: "Jim",
+            },
+            {
+              type: "FamilyName",
+              value: "Ferguson",
+            },
+          ],
+        },
+      ],
+    },
+    type: ["VerifiableCredential", "IdentityCheckCredential"],
     "@context": [
       "https://www.w3.org/2018/credentials/v1",
       "https://vocab.london.cloudapps.digital/contexts/identity-v1.jsonld",
     ],
-    credentialSubject: {
-      name: [
-        {
-          nameParts: [
-            { type: "GivenName", value: "Jim" },
-            { type: "FamilyName", value: "Ferguson" },
-          ],
-        },
-      ],
-      socialSecurityRecord: [{ personalNumber: "AA000003D" }],
-    },
-    evidence: [
-      {
-        checkDetails: [
-          { checkMethod: "data", identityCheckPolicy: "published" },
-        ],
-        strengthScore: 2,
-        txn: expect.any(String),
-        type: "IdentityCheck",
-        validityScore: 2,
-      },
-    ],
-    type: ["VerifiableCredential", "IdentityCheckCredential"],
   },
+  sub: "test",
+  nbf: 1709560618,
+  iss: "0976c11e-8ef3-4659-b7f2-ee0b842b85bd",
+  exp: 1709567818,
+  jti: "urn:uuid:27dd61c4-b91e-4cb8-9c14-65e22d164b1e",
 };
 
 describe("nino-issue-credential-happy", () => {
@@ -74,6 +89,6 @@ describe("nino-issue-credential-happy", () => {
     ).jwt.split(".");
 
     const payload = JSON.parse(decode(payloadEncoded));
-    expect(payload).toEqual(expect.objectContaining(expectedPayload));
+    expect(payload).toEqual(expectedPayload);
   });
 });
