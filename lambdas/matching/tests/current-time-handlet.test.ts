9b92cd547ef8d94691eba19ac6938368a402130c
Connor Johnson <connor.johnson@digital.cabinet-office.gov.uk>
2024-03-25 09:38:11 +0000

    OJ-2337: Added test for current time lambda.

---
+++ lambdas/matching/tests/current-time-handlet.test.ts
@@ -0,0 +1,23 @@
+import { Context } from "aws-lambda";
+import { CurrentTimeHandler } from "../src/current-time-handler";
+
+describe("current-time-handler", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.resetAllMocks();
+  });
+
+  it("should return current time in expected format", async () => {
+    const mockTimestamp = 1622502000000; // 31/05/2021
+    jest.spyOn(Date, "now").mockReturnValue(mockTimestamp);
+
+    const handler = new CurrentTimeHandler();
+    const result = await handler.handler({} as unknown, {} as Context);
+
+
+    expect(result).toEqual({
+      milliseconds: mockTimestamp,
+      seconds: Math.floor(mockTimestamp/1000)
+    });
+  });
+});
