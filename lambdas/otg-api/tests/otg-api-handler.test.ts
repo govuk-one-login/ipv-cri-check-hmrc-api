da085cab3f309c7e18a7d45c7913388a53e843dd
Suraj Kumar <sk96.uk@gmail.com>
2024-04-09 17:20:49 +0100

    OJ-2490: Unit test

---
+++ lambdas/otg-api/tests/otg-api-handler.test.ts
@@ -0,0 +1,74 @@
+import { Context } from "aws-lambda";
+import { OTGApiHandler } from "../src/otg-api-handler";
+
+describe("otg-api-handler", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.resetAllMocks();
+  });
+
+  it("should return a token and expiry", async () => {
+    const mockToken = "goodToken";
+    const mockExpiry = Date.now() + 600000;
+
+    global.fetch = jest.fn();
+    (global.fetch as jest.Mock).mockResolvedValueOnce({
+      json: jest.fn().mockResolvedValueOnce({
+        token: mockToken,
+        expiry: mockExpiry
+      }),
+      ok: true
+    });
+
+    const otgApiHandler = new OTGApiHandler();
+    const event = {
+      apiURL: "https://apigwId-vpceId.execute-api.eu-west-2.amazonaws.com/dev/token/?tokenType=stub",
+    };
+    const result = await otgApiHandler.handler(event, {} as Context);
+    expect(result.token).toBe(mockToken);
+    expect(result.expiry).toBe(mockExpiry);
+  });
+
+  it("should throw when an invalid response is returned from OTG", async () => {
+    global.fetch = jest.fn();
+    (global.fetch as jest.Mock).mockResolvedValueOnce({
+      json: jest.fn().mockResolvedValueOnce({}),
+      ok: false,
+      status: 400,
+      statusText: "Forbidden",
+    });
+
+    const otgApiHandler = new OTGApiHandler();
+    const event = {
+      apiURL: "https://apigwId-vpceId.execute-api.eu-west-2.amazonaws.com/dev/token/?tokenType=stub"
+    };
+
+    await expect(() => otgApiHandler.handler(event, {})).rejects.toThrow(
+      new Error("Error response received from OTG 400 Forbidden")
+    );
+  });
+
+  it("should throw when the bearer token has expired", async () => {
+    const mockToken = "goodToken";
+    const mockExpiry = Date.now() - 600000;
+
+    global.fetch = jest.fn();
+    (global.fetch as jest.Mock).mockResolvedValueOnce({
+      json: jest.fn().mockResolvedValueOnce({
+        token: mockToken,
+        expiry: mockExpiry
+      }),
+      ok: true
+    });
+
+    const otgApiHandler = new OTGApiHandler();
+    const event = {
+      apiURL: "https://apigwId-vpceId.execute-api.eu-west-2.amazonaws.com/dev/token/?tokenType=stub"
+    };
+
+    await expect(() => otgApiHandler.handler(event, {})).rejects.toThrow(
+      new Error("OTG returned an expired Bearer Token")
+    );
+  });
+
+});
