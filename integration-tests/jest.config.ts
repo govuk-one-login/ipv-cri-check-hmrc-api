6b49dd0bea4975306bbc62492328f37adbe6981e
kiejgordon <kieron.gordon@digital.cabinet-office.gov.uk>
2024-04-24 11:45:09 +0100

    Updated jest.config

--- integration-tests/jest.config.ts
+++ integration-tests/jest.config.ts
@@ -3,7 +3,7 @@ import baseConfig from "../jest.config.base";
 
 export default {
   ...baseConfig,
-  projects: ["*/jest.config.ts"],
+  projects: ["*/*/jest.config.ts", "*/jest.config.ts"],
   testMatch: ["<rootDir>/**/*.test.ts"],
   collectCoverage: false,
   modulePaths: [],
