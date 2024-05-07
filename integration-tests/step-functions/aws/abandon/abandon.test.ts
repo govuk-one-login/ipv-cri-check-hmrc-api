227660b0eefcabd85cc64ad1103fe1b2090b261c
Olakunle Jegede <olakunle.jegede@digital.cabinet-office.gov.uk>
2024-05-07 15:08:38 +0100

    OJ-2421: AWS Test of Step Function publish integration
    When Step Function publish data for audit event in we don't
    know if they work, Eventbridge lacks a storage mechanism.
    The idea behind this PR is to temporarily adds test queues
    as a target to existing rules and eventbridge. The test can then assert
    on messages that land in the test queue. The data payload can be
    inspected in the test to ensure the right items are sent.
    Any changes can be caught of things change in future.

--- integration-tests/step-functions/aws/abandon/abandon.test.ts
+++ integration-tests/step-functions/aws/abandon/abandon.test.ts
@@ -1,66 +1,183 @@
+import { CreateQueueCommandOutput } from "@aws-sdk/client-sqs";
 import { stackOutputs } from "../../../resources/cloudformation-helper";
 import {
   clearItems,
   getItemByKey,
   populateTable,
 } from "../../../resources/dynamodb-helper";
+import {
+  addQueuePolicy,
+  createQueue,
+  deleteQueue,
+  getQueueArn,
+  getQueueMessages,
+} from "../../../resources/queue-helper";
 import { executeStepFunction } from "../../../resources/stepfunction-helper";
+import {
+  attachTargetToRule,
+  removeTargetFromRule,
+} from "../../../resources/event-bridge-helper";
+import { RetryConfig, pause, retry } from "../../../resources/util";
 
-describe("abandon", () => {
+describe("Abandon", () => {
   const input = {
     sessionId: "abandon-test",
   };
-
   let output: Partial<{
     CommonStackName: string;
     AbandonStateMachineArn: string;
   }>;
 
   let sessionTableName: string;
+  describe("Step function execute AuthorizationCode is undefined", () => {
+    beforeAll(async () => {
+      output = await stackOutputs(process.env.STACK_NAME);
+      sessionTableName = `session-${output.CommonStackName}`;
+    });
 
-  beforeAll(async () => {
-    output = await stackOutputs(process.env.STACK_NAME);
-    sessionTableName = `session-${output.CommonStackName}`;
-  });
+    afterEach(async () => {
+      await clearItems(sessionTableName, {
+        sessionId: input.sessionId,
+      });
+    });
 
-  afterEach(async () => {
-    await clearItems(sessionTableName, {
-      sessionId: input.sessionId,
+    it("should remove the authorizationCode when session exists", async () => {
+      await populateTable(sessionTableName, {
+        sessionId: input.sessionId,
+        expiryDate: 9999999999,
+        clientId: "exampleClientId",
+        authorizationCode: "9999999999",
+        authorizationCodeExpiryDate: "9999999999",
+        clientSessionId: "252561a2-c6ef-47e7-87ab-93891a2a6a41",
+        persistentSessionId: "156714ef-f9df-48c2-ada8-540e7bce44f7",
+        clientIpAddress: "00.100.8.20",
+        subject: "test",
+      });
+
+      const startExecutionResult = await executeStepFunction(
+        output.AbandonStateMachineArn as string,
+        input
+      );
+
+      const sessionRecord = await getItemByKey(sessionTableName, {
+        sessionId: input.sessionId,
+      });
+
+      expect(startExecutionResult.output).toBe('{"httpStatus":200}');
+      expect(sessionRecord.Item?.authorizationCode).toBeUndefined();
+      expect(sessionRecord.Item?.authorizationCodeExpiryDate).toBe(0);
+    });
+
+    it("should return a 400 when session does not exist", async () => {
+      const startExecutionResult = await executeStepFunction(
+        output.AbandonStateMachineArn as string,
+        input
+      );
+      expect(startExecutionResult.output).toBe('{"httpStatus":400}');
     });
   });
 
-  it("should remove the authorizationCode when session exists", async () => {
-    await populateTable(sessionTableName, {
-      sessionId: input.sessionId,
-      expiryDate: 9999999999,
-      clientId: "exampleClientId",
-      authorizationCode: "9999999999",
-      authorizationCodeExpiryDate: "9999999999",
-      clientSessionId: "252561a2-c6ef-47e7-87ab-93891a2a6a41",
-      persistentSessionId: "156714ef-f9df-48c2-ada8-540e7bce44f7",
-      clientIpAddress: "00.100.8.20",
-      subject: "test",
+  describe("Step Function execute data publish for abandon event on queue", () => {
+    jest.setTimeout(60_000);
+
+    const targetId = `queue-target-id${Date.now()}`;
+
+    let output: Partial<{
+      CommonStackName: string;
+      AbandonStateMachineArn: string;
+      AuditEventAbandonedRule: string;
+      AuditEventAbandonedRuleArn: string;
+    }>;
+
+    let sessionTableName: string;
+    let queueResponse: CreateQueueCommandOutput;
+    let queueArn: string | undefined;
+
+    beforeAll(async () => {
+      output = await stackOutputs(process.env.STACK_NAME);
+      sessionTableName = `session-${output.CommonStackName}`;
+      const auditEventAbandonedRule = output?.AuditEventAbandonedRule as string;
+      const [eventBusName, ruleName] = auditEventAbandonedRule.split("|");
+      queueResponse = await createQueue(`event-bus-test-Queue-${Date.now()}`);
+      queueArn = (await getQueueArn(queueResponse.QueueUrl)) as string;
+      await pause(15);
+
+      await addQueuePolicy(
+        queueArn,
+        queueResponse.QueueUrl,
+        output.AuditEventAbandonedRuleArn
+      );
+
+      await pause(15);
+      await attachTargetToRule(targetId, eventBusName, ruleName, queueArn);
+      await pause(15);
     });
 
-    const startExecutionResult = await executeStepFunction(
-      output.AbandonStateMachineArn as string,
-      input
-    );
+    afterEach(async () => {
+      const auditEventAbandonedRule = output?.AuditEventAbandonedRule as string;
+      const [eventBusName, ruleName] = auditEventAbandonedRule.split("|");
 
-    const sessionRecord = await getItemByKey(sessionTableName, {
-      sessionId: input.sessionId,
+      await clearItems(sessionTableName, {
+        sessionId: input.sessionId,
+      });
+      await retry(
+        { intervalInMs: 500, maxRetries: 20 },
+        async () => await removeTargetFromRule(targetId, eventBusName, ruleName)
+      );
+      await retry(
+        { intervalInMs: 500, maxRetries: 20 },
+        async () => await deleteQueue(queueResponse.QueueUrl)
+      );
     });
 
-    expect(startExecutionResult.output).toBe('{"httpStatus":200}');
-    expect(sessionRecord.Item?.authorizationCode).toBeUndefined();
-    expect(sessionRecord.Item?.authorizationCodeExpiryDate).toBe(0);
-  });
+    it("should publish data for an abandon event", async () => {
+      await populateTable(sessionTableName, {
+        sessionId: input.sessionId,
+        expiryDate: 9999999999,
+        clientId: "exampleClientId",
+        authorizationCode: "9999999999",
+        authorizationCodeExpiryDate: "9999999999",
+        clientSessionId: "252561a2-c6ef-47e7-87ab-93891a2a6a41",
+        persistentSessionId: "156714ef-f9df-48c2-ada8-540e7bce44f7",
+        clientIpAddress: "00.100.8.20",
+        subject: "test",
+      });
 
-  it("should return a 400 when session does not exist", async () => {
-    const startExecutionResult = await executeStepFunction(
-      output.AbandonStateMachineArn as string,
-      input
-    );
-    expect(startExecutionResult.output).toBe('{"httpStatus":400}');
+      const startExecutionResult = await executeStepFunction(
+        output.AbandonStateMachineArn as string,
+        input
+      );
+
+      const messages = await getQueueMessages(
+        queueResponse.QueueUrl as string,
+        {
+          intervalInMs: 0,
+          maxRetries: 3,
+        } as RetryConfig
+      );
+      const {
+        "detail-type": detailType,
+        source,
+        detail,
+      } = JSON.parse(messages[0].Body as string);
+
+      expect(startExecutionResult.output).toBe('{"httpStatus":200}');
+
+      expect(queueResponse).toBeDefined();
+      expect(queueArn).toBeDefined();
+      expect(detailType).toBe("ABANDONED");
+      expect(source).toBe("review-hc.localdev.account.gov.uk");
+      expect(detail).toEqual({
+        auditPrefix: "IPV_HMRC_RECORD_CHECK_CRI",
+        user: {
+          govuk_signin_journey_id: "252561a2-c6ef-47e7-87ab-93891a2a6a41",
+          user_id: "test",
+          persistent_session_id: "156714ef-f9df-48c2-ada8-540e7bce44f7",
+          session_id: "abandon-test",
+          ip_address: "00.100.8.20",
+        },
+        issuer: "https://review-hc.dev.account.gov.uk",
+      });
+    });
   });
 });
--- integration-tests/step-functions/aws/nino-check/nino-check-happy.test.ts
+++ integration-tests/step-functions/aws/nino-check/nino-check-happy.test.ts
@@ -5,6 +5,15 @@ import {
   clearAttemptsTable,
   populateTables,
 } from "../../../resources/dynamodb-helper";
+import { CreateQueueCommandOutput } from "@aws-sdk/client-sqs";
+import {
+  deleteQueue,
+  getQueueMessages,
+  setUpQueueAndAttachToRule,
+  targetId,
+} from "../../../resources/queue-helper";
+import { removeTargetFromRule } from "../../../resources/event-bridge-helper";
+import { RetryConfig, retry } from "../../../resources/util";
 
 jest.setTimeout(30_000);
 
@@ -29,6 +38,10 @@ describe("nino-check-happy", () => {
     UserAttemptsTable: string;
     NinoUsersTable: string;
     NinoCheckStateMachineArn: string;
+    AuditEventRequestSentRule: string;
+    AuditEventResponseReceivedRule: string;
+    AuditEventRequestSentRuleArn: string;
+    AuditEventResponseReceivedRuleArn: string;
   }>;
 
   beforeEach(async () => {
@@ -107,79 +120,252 @@ describe("nino-check-happy", () => {
     await clearAttemptsTable("check-unhappy", output.UserAttemptsTable);
   });
 
-  it("should execute nino step function 1st attempt", async () => {
-    const startExecutionResult = await executeStepFunction(
-      output.NinoCheckStateMachineArn as string,
-      input
-    );
+  describe("Step Function success", () => {
+    it("should return 200 Ok on 1st attempt", async () => {
+      const startExecutionResult = await executeStepFunction(
+        output.NinoCheckStateMachineArn as string,
+        input
+      );
 
-    expect(startExecutionResult.output).toBe('{"httpStatus":200}');
+      expect(startExecutionResult.output).toBe('{"httpStatus":200}');
+    });
+
+    it("should return 200 Ok while retrying the 2nd attempt", async () => {
+      const inputNoCidNinoUser = {
+        sessionId: "check-unhappy",
+        nino: "AA000003C",
+      };
+      const testNoCidNinoUser = {
+        nino: inputNoCidNinoUser.nino,
+        dob: "1948-04-23",
+        firstName: "Error",
+        lastName: "NinoDoesNotMatchCID",
+      };
+
+      await populateTables(
+        {
+          tableName: sessionTableName,
+          items: {
+            sessionId: inputNoCidNinoUser.sessionId,
+            expiryDate: 9999999999,
+            clientId: "ipv-core-stub-aws-prod",
+            clientSessionId: "252561a2-c6ef-47e7-87ab-93891a2a6a41",
+            persistentSessionId: "156714ef-f9df-48c2-ada8-540e7bce44f7",
+            subject: "test",
+            clientIpAddress: "00.100.8.20",
+          },
+        },
+        {
+          tableName: personIdentityTableName,
+          items: {
+            sessionId: inputNoCidNinoUser.sessionId,
+            nino: inputNoCidNinoUser.sessionId,
+            birthDates: [{ value: testNoCidNinoUser.dob }],
+            names: [
+              {
+                nameParts: [
+                  {
+                    type: "GivenName",
+                    value: testNoCidNinoUser.firstName,
+                  },
+                  {
+                    type: "FamilyName",
+                    value: testNoCidNinoUser.lastName,
+                  },
+                ],
+              },
+            ],
+          },
+        }
+      );
+
+      const firstExecutionResult = await executeStepFunction(
+        output.NinoCheckStateMachineArn as string,
+        {
+          sessionId: inputNoCidNinoUser.sessionId,
+          nino: testNoCidNinoUser.nino,
+        }
+      );
+
+      const secondExecutionResult = await executeStepFunction(
+        output.NinoCheckStateMachineArn as string,
+        input
+      );
+
+      expect(firstExecutionResult.output).toBe('{"httpStatus":422}');
+
+      expect(secondExecutionResult.output).toBe('{"httpStatus":200}');
+    });
   });
 
-  it("should execute nino step function 2nd attempt", async () => {
-    const inputNoCidNinoUser = {
-      sessionId: "check-unhappy",
-      nino: "AA000003C",
-    };
-    const testNoCidNinoUser = {
-      nino: inputNoCidNinoUser.nino,
-      dob: "1948-04-23",
-      firstName: "Error",
-      lastName: "NinoDoesNotMatchCID",
-    };
+  describe("Nino Hmrc Check Step Function execution", () => {
+    jest.setTimeout(120_000);
+    let requestSentEventTestQueue: CreateQueueCommandOutput;
+    let responseReceivedEventTestQueue: CreateQueueCommandOutput;
 
-    await populateTables(
-      {
-        tableName: sessionTableName,
-        items: {
-          sessionId: inputNoCidNinoUser.sessionId,
-          expiryDate: 9999999999,
-          clientId: "ipv-core-stub-aws-prod",
-          clientSessionId: "252561a2-c6ef-47e7-87ab-93891a2a6a41",
-          persistentSessionId: "156714ef-f9df-48c2-ada8-540e7bce44f7",
-          subject: "test",
-          clientIpAddress: "00.100.8.20",
+    beforeEach(async () => {
+      const [requestSentBusName, requestSentRuleName] = (
+        output.AuditEventRequestSentRule as string
+      ).split("|");
+      const [responseReceivedBusName, responseReceivedRuleName] = (
+        output.AuditEventResponseReceivedRule as string
+      ).split("|");
+
+      requestSentEventTestQueue = await setUpQueueAndAttachToRule(
+        output.AuditEventRequestSentRuleArn as string,
+        requestSentRuleName,
+        requestSentBusName
+      );
+      responseReceivedEventTestQueue = await setUpQueueAndAttachToRule(
+        output.AuditEventResponseReceivedRuleArn as string,
+        responseReceivedRuleName,
+        responseReceivedBusName
+      );
+    });
+
+    afterEach(async () => {
+      const [requestSentBusName, requestSentRuleName] = (
+        output.AuditEventRequestSentRule as string
+      ).split("|");
+      const [responseReceivedBusName, responseReceivedRuleName] = (
+        output.AuditEventResponseReceivedRule as string
+      ).split("|");
+
+      await retry({ intervalInMs: 1000, maxRetries: 20 }, async () => {
+        await removeTargetFromRule(
+          targetId,
+          requestSentBusName,
+          requestSentRuleName
+        );
+        await removeTargetFromRule(
+          targetId,
+          responseReceivedBusName,
+          responseReceivedRuleName
+        );
+      });
+      await retry({ intervalInMs: 1000, maxRetries: 20 }, async () => {
+        await deleteQueue(requestSentEventTestQueue.QueueUrl);
+        await deleteQueue(responseReceivedEventTestQueue.QueueUrl);
+      });
+    });
+    it("publishes REQUEST_SENT and RESPONSE_RECEIVED events successfully", async () => {
+      const startExecutionResult = await executeStepFunction(
+        output.NinoCheckStateMachineArn as string,
+        input
+      );
+      const requestSentQueueMessage = await getQueueMessages(
+        requestSentEventTestQueue.QueueUrl as string,
+        {
+          intervalInMs: 0,
+          maxRetries: 10,
+        } as RetryConfig
+      );
+      const responseReceivedQueueMessage = await getQueueMessages(
+        responseReceivedEventTestQueue.QueueUrl as string,
+        {
+          intervalInMs: 0,
+          maxRetries: 10,
+        } as RetryConfig
+      );
+      const {
+        "detail-type": requestSentDetailType,
+        source: requestSentSource,
+        detail: requestSentDetail,
+      } = JSON.parse(requestSentQueueMessage[0].Body as string);
+      const {
+        "detail-type": responseReceivedDetailType,
+        source: responseReceivedSource,
+        detail: responseReceivedDetail,
+      } = JSON.parse(responseReceivedQueueMessage[0].Body as string);
+
+      expect(startExecutionResult.output).toBe('{"httpStatus":200}');
+
+      expect(startExecutionResult.output).toBeDefined();
+      expect(requestSentDetailType).toBe("REQUEST_SENT");
+      expect(requestSentSource).toBe("review-hc.localdev.account.gov.uk");
+      expect(requestSentDetail).toEqual({
+        auditPrefix: "IPV_HMRC_RECORD_CHECK_CRI",
+        nino: "AA000003D",
+        user: {
+          govuk_signin_journey_id: "252561a2-c6ef-47e7-87ab-93891a2a6a41",
+          user_id: "test",
+          persistent_session_id: "156714ef-f9df-48c2-ada8-540e7bce44f7",
+          session_id: "check-happy",
+          ip_address: "00.100.8.20",
         },
-      },
-      {
-        tableName: personIdentityTableName,
-        items: {
-          sessionId: inputNoCidNinoUser.sessionId,
-          nino: inputNoCidNinoUser.sessionId,
-          birthDates: [{ value: testNoCidNinoUser.dob }],
-          names: [
+        userInfoEvent: {
+          Count: 1,
+          Items: [
             {
-              nameParts: [
-                {
-                  type: "GivenName",
-                  value: testNoCidNinoUser.firstName,
-                },
-                {
-                  type: "FamilyName",
-                  value: testNoCidNinoUser.lastName,
-                },
-              ],
+              names: {
+                L: [
+                  {
+                    M: {
+                      nameParts: {
+                        L: [
+                          {
+                            M: {
+                              type: {
+                                S: "GivenName",
+                              },
+                              value: {
+                                S: "Jim",
+                              },
+                            },
+                          },
+                          {
+                            M: {
+                              type: {
+                                S: "FamilyName",
+                              },
+                              value: {
+                                S: "Ferguson",
+                              },
+                            },
+                          },
+                        ],
+                      },
+                    },
+                  },
+                ],
+              },
+              sessionId: {
+                S: "check-happy",
+              },
+              birthDates: {
+                L: [
+                  {
+                    M: {
+                      value: {
+                        S: "1948-04-23",
+                      },
+                    },
+                  },
+                ],
+              },
+              nino: {
+                S: "AA000003D",
+              },
             },
           ],
+          ScannedCount: 1,
         },
-      }
-    );
+        issuer: "https://review-hc.dev.account.gov.uk",
+      });
 
-    const firstExecutionResult = await executeStepFunction(
-      output.NinoCheckStateMachineArn as string,
-      {
-        sessionId: inputNoCidNinoUser.sessionId,
-        nino: testNoCidNinoUser.nino,
-      }
-    );
-
-    const secondExecutionResult = await executeStepFunction(
-      output.NinoCheckStateMachineArn as string,
-      input
-    );
-
-    expect(firstExecutionResult.output).toBe('{"httpStatus":422}');
-
-    expect(secondExecutionResult.output).toBe('{"httpStatus":200}');
+      expect(responseReceivedDetailType).toBe("RESPONSE_RECEIVED");
+      expect(responseReceivedSource).toBe("review-hc.localdev.account.gov.uk");
+      expect(responseReceivedDetail).toEqual({
+        auditPrefix: "IPV_HMRC_RECORD_CHECK_CRI",
+        user: {
+          govuk_signin_journey_id: "252561a2-c6ef-47e7-87ab-93891a2a6a41",
+          user_id: "test",
+          persistent_session_id: "156714ef-f9df-48c2-ada8-540e7bce44f7",
+          session_id: "check-happy",
+          ip_address: "00.100.8.20",
+        },
+        issuer: "https://review-hc.dev.account.gov.uk",
+      });
+    });
   });
 });
