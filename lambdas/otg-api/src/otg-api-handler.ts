09d6c29fff6786e140738f5c4c918203eabb5cff
Suraj Kumar <sk96.uk@gmail.com>
2024-05-23 16:33:23 +0100

    OJ-2310: Log journey ID in otg function

--- lambdas/otg-api/src/otg-api-handler.ts
+++ lambdas/otg-api/src/otg-api-handler.ts
@@ -5,29 +5,37 @@ const logger = new Logger();
 
 export class OTGApiHandler implements LambdaInterface {
   public async handler(
-    event: { apiURL: string },
+    event: { apiURL: string; govJourneyId: string },
     _context: unknown
-  ): Promise<{ token: string, expiry: number }> {
+  ): Promise<{ token: string; expiry: number }> {
+    logger.info(
+      `Lambda invoked with government journey id: ${event.govJourneyId}`
+    );
+
     try {
       const response = await fetch(event.apiURL, {
         method: "GET",
       });
 
-      if(response.ok) {
+      if (response.ok) {
         const body = await response.json();
         const expiry = body.expiry;
         const now = Date.now();
-        if(now > expiry) {
-          throw new Error("OTG returned an expired Bearer Token")
+        if (now > expiry) {
+          throw new Error("OTG returned an expired Bearer Token");
         }
         return body;
       }
 
       throw new Error(
-        `Error response received from OTG ${response.status} ${response.statusText}`)
+        `Error response received from OTG ${response.status} ${response.statusText}`
+      );
     } catch (error: unknown) {
-      const message = (error instanceof Error) ? error.message : String(error);
-      logger.error("Error in OTGApiHandler: " + message);
+      const message = error instanceof Error ? error.message : String(error);
+      logger.error({
+        message: "Error in OTGApiHandler: " + message,
+        govJourneyId: event.govJourneyId,
+      });
       throw error;
     }
   }
