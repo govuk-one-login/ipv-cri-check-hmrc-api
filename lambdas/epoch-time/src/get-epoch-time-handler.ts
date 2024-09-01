a5eace17bc98422c3aa1be8f7a316cf284721b85
Olakunle Jegede <olakunle.jegede@digital.cabinet-office.gov.uk>
2024-09-01 16:22:22 +0100

    chore: Refactor get-epoch-time-handler
    Make get-epoch-time handler a little more like the other
    time handlers, i.e. don't pass the time string from the statemachine
    i.e $$.Execution.EnteredTime
    
    Make the handler return an object the contains the epoch seconds and
    milliseconds
    
    Refactor the audit_event statemachine to use just one epoch time handler
    instead of two

--- lambdas/epoch-time/src/get-epoch-time-handler.ts
+++ lambdas/epoch-time/src/get-epoch-time-handler.ts
@@ -2,48 +2,22 @@ import { LambdaInterface } from "@aws-lambda-powertools/commons";
 import { LogHelper } from "../../logging/log-helper";
 import { Context } from "aws-lambda";
 
-export type TimeUnit = "seconds" | "milliseconds";
-
 interface EpochTimeEvent {
-  dateTime: string;
   govJourneyId: string;
-  unit?: TimeUnit;
 }
 
 const logHelper = new LogHelper();
 
 export class EpochTimeHandler implements LambdaInterface {
-  public async handler(
-    event: EpochTimeEvent,
-    context: Context
-  ): Promise<number> {
+  public async handler(event: EpochTimeEvent, context: Context) {
     logHelper.logEntry(context.functionName, event.govJourneyId);
-    if (!event.dateTime) {
-      throw new Error("Invalid event object: missing dateTime");
-    }
-    const unit = event.unit ?? "seconds";
-    const timestamp = new Date(event.dateTime).getTime();
 
-    if (isNaN(timestamp)) {
-      const errorMessage = "Invalid date format";
-      logHelper.logError(
-        context.functionName,
-        event.govJourneyId,
-        errorMessage
-      );
-      throw new Error(errorMessage);
-    }
-    if (!["seconds", "milliseconds"].includes(unit)) {
-      const errorMessage = `Invalid unit value: ${unit}`;
-      logHelper.logError(
-        context.functionName,
-        event.govJourneyId,
-        errorMessage
-      );
-      throw new Error(errorMessage);
-    }
+    const timestamp = Date.now();
 
-    return unit === "milliseconds" ? timestamp : Math.floor(timestamp / 1000);
+    return {
+      milliseconds: timestamp,
+      seconds: Math.floor(timestamp / 1000),
+    };
   }
 }
 
--- lambdas/epoch-time/tests/get-epoch-time-handler.test.ts
+++ lambdas/epoch-time/tests/get-epoch-time-handler.test.ts
@@ -1,76 +1,22 @@
-import { TimeUnit, lambdaHandler } from "../src/get-epoch-time-handler";
+import { lambdaHandler } from "../src/get-epoch-time-handler";
 import { Context } from "aws-lambda";
 
 const mockGovJourneyId = "test-government-journey-id";
 describe("lambdaHandler", () => {
-  it("returns the seconds representation of the provided date", async () => {
-    const event = {
-      dateTime: "2024-02-26T12:00:00Z",
-      govJourneyId: mockGovJourneyId,
-    };
+  it("returns an object with epoch secs and milliseconds representation of the provided date", async () => {
+    const expectedMilliseconds = 1708948800000;
     const expectedSeconds = 1708948800;
 
-    const result = await lambdaHandler(event, {} as Context);
-
-    expect(result).toBe(expectedSeconds);
-  });
-
-  it("returns the milliseconds representation of the provided date", async () => {
+    jest.spyOn(Date, "now").mockReturnValue(expectedMilliseconds);
     const event = {
-      dateTime: "2024-02-26T12:00:00Z",
-      unit: "milliseconds" as TimeUnit,
-      govJourneyId: mockGovJourneyId,
-    };
-    const expectedMilliseconds = 1708948800000;
-
-    const result = await lambdaHandler(event, {} as Context);
-
-    expect(result).toBe(expectedMilliseconds);
-  });
-
-  it("returns the milliseconds representation of the provided date", async () => {
-    const event = {
-      dateTime: "invalidDateTimeFormat",
-      unit: "milliseconds" as TimeUnit,
       govJourneyId: mockGovJourneyId,
     };
 
-    expect(
-      async () => await lambdaHandler(event, {} as Context)
-    ).rejects.toThrow(new Error("Invalid date format"));
+    const result = await lambdaHandler(event, {} as Context);
+
+    expect(result).toEqual({
+      milliseconds: expectedMilliseconds,
+      seconds: expectedSeconds,
+    });
   });
-
-  it.each([undefined, null])(
-    "throws missing dateTime error when dateTime argument is %s",
-    async (value) => {
-      expect(
-        async () =>
-          await lambdaHandler(
-            {
-              dateTime: value as unknown as string,
-              unit: "milliseconds" as TimeUnit,
-              govJourneyId: mockGovJourneyId,
-            },
-            {} as Context
-          )
-      ).rejects.toThrow(new Error("Invalid event object: missing dateTime"));
-    }
-  );
-
-  it.each(["minutes, hours"])(
-    "throws invalid unit value when unit argument is %s",
-    async (value) => {
-      expect(
-        async () =>
-          await lambdaHandler(
-            {
-              dateTime: "2024-02-26T12:00:00Z",
-              unit: value as TimeUnit,
-              govJourneyId: mockGovJourneyId,
-            },
-            {} as Context
-          )
-      ).rejects.toThrow(new Error(`Invalid unit value: ${value}`));
-    }
-  );
 });
