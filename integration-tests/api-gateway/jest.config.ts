1c2a131ad9f4cca09033b3ff302437d91ee2b33e
Olakunle Jegede <olakunle.jegede@digital.cabinet-office.gov.uk>
2025-04-08 08:17:47 +0100

    OJ-3131: Added test flags to reduce flakyness
    Set maxWorkers to run tests on main thread without spawning
    working processes
    Detects unclosed resources (like dynamodb connections, network)
    that prevent Jest from exiting cleanly

--- integration-tests/api-gateway/jest.config.ts
+++ integration-tests/api-gateway/jest.config.ts
@@ -4,5 +4,10 @@ import baseConfig from "../jest.config";
 export default {
   ...baseConfig,
   projects: [],
+  maxWorkers: 1,
+  detectOpenHandles: true,
   displayName: "integration-tests/api-gateway",
+  globalSetup: "<rootDir>/api-test-context/setup-integration-context.ts",
+  globalTeardown:
+    "<rootDir>/api-test-context/setup-integration-context-tear-down.ts",
 } satisfies Config;
