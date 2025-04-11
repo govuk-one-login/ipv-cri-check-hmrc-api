9d30cc087fcc7b2d48a331939a71c0aa74fae8f6
Olakunle Jegede <olakunle.jegede@digital.cabinet-office.gov.uk>
2025-04-11 06:54:31 +0100

    OJ-3131: Removed unused crypto functions

--- integration-tests/api-gateway/crypto/create-jar-request-payload.ts
+++ integration-tests/api-gateway/crypto/create-jar-request-payload.ts
@@ -1,5 +1,4 @@
 import { JWTPayload } from "jose";
-import { buildJarAuthorizationRequest } from "./client";
 
 export type PrivateSigningKey = {
   kty: string;
@@ -21,13 +20,4 @@ export type Payload = {
   privateSigningKey: PrivateSigningKey;
   issuer: string;
   claimSet: JWTPayload;
-};
-
-export const getJarAuthorizationPayload = async (payload: Payload) => {
-  try {
-    return await buildJarAuthorizationRequest(payload);
-  } catch (error) {
-    console.error("Error building Jar Authorization Request:", error);
-    return null;
-  }
 };
