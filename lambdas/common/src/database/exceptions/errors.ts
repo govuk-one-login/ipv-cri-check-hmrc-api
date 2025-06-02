0f101217167c836411c1881e820e8364bce06fe9
Barnaby Collins <23247109+barnabycollins@users.noreply.github.com>
2025-06-02 13:43:21 +0100

    OJ-3192 - recordName -> tableName

--- lambdas/common/src/database/exceptions/errors.ts
+++ lambdas/common/src/database/exceptions/errors.ts
@@ -1,6 +1,6 @@
 export class RecordExpiredError extends Error {
   constructor(
-    public readonly recordName: string,
+    public readonly tableName: string,
     public readonly sessionId: string,
     public readonly expiryDates: number[]
   ) {
@@ -8,9 +8,9 @@ export class RecordExpiredError extends Error {
   }
 
   get message() {
-    return `Found only expired records for ${this.recordName} on sessionId ${
-      this.sessionId
-    }: ${this.expiryDates
+    return `Found only expired records on the ${
+      this.tableName
+    } table for sessionId ${this.sessionId}: ${this.expiryDates
       // multiply by 1000 as expiryDate is in Unix seconds but Date expects milliseconds
       .map((r) => new Date(r * 1000).toISOString())
       .join(", ")}.`;
@@ -19,13 +19,13 @@ export class RecordExpiredError extends Error {
 
 export class RecordNotFoundError extends Error {
   constructor(
-    public readonly recordName: string,
+    public readonly tableName: string,
     public readonly sessionId: string
   ) {
     super();
   }
 
   get message() {
-    return `Failed to find a valid entry in the ${this.recordName} table with session ID ${this.sessionId}.`;
+    return `Failed to find a valid entry in the ${this.tableName} table with session ID ${this.sessionId}.`;
   }
 }
