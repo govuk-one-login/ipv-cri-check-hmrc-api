6afbb3d059c42ccf763931d1d9d734234bb195ed
mikebeeby <169169718+mikebeeby@users.noreply.github.com>
2025-06-19 11:16:01 +0100

    Revert "OJ-3194: Create abandon lambda"

--- lambdas/common/src/errors/cri-error.ts
+++
@@ -1,9 +0,0 @@
-export class CriError extends Error {
-  status: number;
-
-  constructor(status: number, message: string) {
-    super(message);
-    this.status = status;
-    this.name = "CriError";
-  }
-}
