caf58dc3138b1d77c6edd2f03b5c71d093313aa1
Suraj Kumar <sk96.uk@gmail.com>
2025-06-26 14:31:39 +0100

    test: add integration test
    refs: OJ-3178

---
+++ integration-tests/api-gateway/jwks-endpoint/jwks-endpoint.test.ts
@@ -0,0 +1,30 @@
+import { environment } from "../env-variables";
+
+let endpointURL: string;
+
+jest.setTimeout(30_000);
+
+describe("/.well-known/jwks.json", () => {
+  beforeAll(async () => {
+    endpointURL = `https://${process.env.PUBLIC_API}.execute-api.eu-west-2.amazonaws.com/${environment}/.well-known/jwks.json`;
+  });
+
+  it("should contain public JWK key set", async () => {
+    const response = await fetch(endpointURL, {
+      method: "GET",
+    });
+
+    const status = response.status;
+    const body = await response.json();
+
+    expect(status).toEqual(200);
+    expect(body.keys).toBeDefined();
+    expect(body.keys.length).toBeGreaterThan(0);
+
+    expect(body.keys[0].kty).toEqual("RSA");
+    expect(body.keys[0].n).toBeDefined();
+    expect(body.keys[0].use).toEqual("enc");
+    expect(body.keys[0].kid).toBeDefined();
+    expect(body.keys[0].alg).toEqual("RSA_OAEP_256");
+  });
+});
