47799a30ba8d3f60c7f9ecce898f73e9fc9f1ae0
Olakunle Jegede <olakunle.jegede@digital.cabinet-office.gov.uk>
2025-07-28 15:44:40 +0100

    OJ-3268: Separate Environ config from param config

--- lambdas/issue-credential/src/config/function-config.ts
+++ lambdas/issue-credential/src/config/function-config.ts
@@ -1,9 +1,5 @@
 import { BaseFunctionConfig } from "../../../common/src/config/base-function-config";
-import { CriError } from "../../../common/src/errors/cri-error";
 import { TimeUnits } from "../../../common/src/util/date-time";
-import { getParametersValues } from "../../../common/src/util/get-parameters";
-import { logger } from "../../../common/src/util/logger";
-import { CiReasonsMapping } from "../vc/contraIndicator/types/ci-reasons-mapping";
 
 const envVarNames = {
   maxJwtTtl: "MAX_JWT_TTL",
@@ -15,32 +11,6 @@ export type CredentialIssuerEnv = {
   jwtTtlUnit: string;
   commonStackName: string;
 };
-export type VcCheckConfig = {
-  readonly kms: { signingKeyId: string };
-  readonly contraIndicator: { errorMapping: string[]; reasonsMapping: CiReasonsMapping[] };
-};
-const cacheTtlInSeconds = Number(process.env.POWERTOOLS_PARAMETERS_MAX_AGE) || 300;
-
-export const getVcConfig = async (commonStackName: string): Promise<VcCheckConfig> => {
-  const vcSigningKeyId = `/${commonStackName}/verifiableCredentialKmsSigningKeyId`;
-  const errorMapping = "/check-hmrc-cri-api/contraindicationMappings";
-  const reasonsMapping = "/check-hmrc-cri-api/contraIndicatorReasonsMapping";
-  try {
-    const ssmParams = await getParametersValues([vcSigningKeyId, errorMapping, reasonsMapping], cacheTtlInSeconds);
-    logger.info("Retrieved Check Hmrc VC parameters.");
-    return {
-      kms: { signingKeyId: ssmParams[vcSigningKeyId] },
-      contraIndicator: {
-        errorMapping: ssmParams[errorMapping].split("||"),
-        reasonsMapping: JSON.parse(ssmParams[reasonsMapping]),
-      },
-    };
-  } catch (err: unknown) {
-    const message = err instanceof Error ? err.message : String(err);
-    throw new CriError(500, `Failed to load VC config: ${message}`);
-  }
-};
-
 export class IssueCredFunctionConfig extends BaseFunctionConfig {
   public readonly credentialIssuerEnv: CredentialIssuerEnv;
 
---
+++ lambdas/issue-credential/src/config/vc-config.ts
@@ -0,0 +1,29 @@
+import { CriError } from "../../../common/src/errors/cri-error";
+import { getParametersValues } from "../../../common/src/util/get-parameters";
+import { logger } from "../../../common/src/util/logger";
+import { CiReasonsMapping } from "../vc/contraIndicator/types/ci-reasons-mapping";
+
+const cacheTtlInSeconds = Number(process.env.POWERTOOLS_PARAMETERS_MAX_AGE) || 300;
+export type VcCheckConfig = {
+  readonly kms: { signingKeyId: string };
+  readonly contraIndicator: { errorMapping: string[]; reasonsMapping: CiReasonsMapping[] };
+};
+export const getVcConfig = async (commonStackName: string): Promise<VcCheckConfig> => {
+  const vcSigningKeyId = `/${commonStackName}/verifiableCredentialKmsSigningKeyId`;
+  const errorMapping = "/check-hmrc-cri-api/contraindicationMappings";
+  const reasonsMapping = "/check-hmrc-cri-api/contraIndicatorReasonsMapping";
+  try {
+    const ssmParams = await getParametersValues([vcSigningKeyId, errorMapping, reasonsMapping], cacheTtlInSeconds);
+    logger.info("Retrieved Check Hmrc VC parameters.");
+    return {
+      kms: { signingKeyId: ssmParams[vcSigningKeyId] },
+      contraIndicator: {
+        errorMapping: ssmParams[errorMapping].split("||"),
+        reasonsMapping: JSON.parse(ssmParams[reasonsMapping]),
+      },
+    };
+  } catch (err: unknown) {
+    const message = err instanceof Error ? err.message : String(err);
+    throw new CriError(500, `Failed to load VC config: ${message}`);
+  }
+};
