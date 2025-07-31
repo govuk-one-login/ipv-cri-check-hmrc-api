88c9f06f83a7202e0fe685caee4494cde78b2e5e
Barnaby Collins <23247109+barnabycollins@users.noreply.github.com>
2025-07-31 16:16:01 +0100

    OJ-3281 - Use SQS for integration / E2E testing; remove EVN tests

---
+++ integration-tests/api-gateway/user.ts
@@ -0,0 +1,14 @@
+export const testUser = {
+  nino: "AA000003D",
+  dob: "1965-07-08",
+  firstName: "Kenneth",
+  lastName: "Decerqueira",
+  formattedName: [
+    {
+      nameParts: [
+        { type: "GivenName", value: "KENNETH" },
+        { type: "FamilyName", value: "DECERQUEIRA" },
+      ],
+    },
+  ],
+};
