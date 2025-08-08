a88aae445e2fb76993335f20925b6f83fa67bb45
Mike Beeby <mike.beeby@digital.cabinet-office.gov.uk>
2025-08-08 12:54:41 +0100

    OJ-3280: Remove PII Redactor lambda and resources

--- lambdas/pii-redact/tests/utils.ts
+++
@@ -1,9 +0,0 @@
-import { Logger } from "@aws-lambda-powertools/logger";
-
-export const mockLogger = {
-  info: jest.fn(),
-  critical: jest.fn(),
-  debug: jest.fn(),
-  error: jest.fn(),
-  warn: jest.fn(),
-} as unknown as Logger;
