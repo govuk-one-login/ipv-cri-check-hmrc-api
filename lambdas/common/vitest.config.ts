14c77c58dde4c2a791a894a92974dfbb48c78505
Suraj Kumar <sk96.uk@gmail.com>
2026-03-25 10:48:52 +0000

    chore: remove unneeded metrics.test.ts

--- lambdas/common/vitest.config.ts
+++ lambdas/common/vitest.config.ts
@@ -4,10 +4,5 @@ import baseConfig from "../../vitest.config.base";
 export default mergeConfig(baseConfig, defineConfig({
   test: {
     name: "lambdas/common",
-    server: {
-      deps: {
-        inline: ["@govuk-one-login/cri-metrics"],
-      },
-    },
   },
 }));
